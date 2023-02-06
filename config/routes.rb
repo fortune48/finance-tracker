76d04eae4b7732d09937065d0f2ab46e4937e3e7
fortune48 <ukwuomafortune01@gmail.com>
2023-02-06 21:31:40 +0100

    implement remove tracking and add friend functionality. Also display user profile and tracking options

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "my_friends", to: "users#my_friends"
   resources :user_stocks
+  resources :friendships
   devise_for :users
   root "welcome#index"
   get 'welcome/index'
@@ -8,5 +9,6 @@ Rails.application.routes.draw do
   get "my_portfolio", to: "users#my_portfolio"
   get "search_stock", to: "stocks#search"
   get "search_friend", to: "users#search"
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
